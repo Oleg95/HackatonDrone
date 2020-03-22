be17a7d1b0ba88de5117f7f676b1b29f62bdafcf
Oleg95 <Prutkin96@mail.ru>
2020-03-22 13:49:50 +0300

    add comments

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import math
 
 pygame.init()
 
-map = pygame.image.load(r'images/abstract_map.jpg')
-display_width, display_height = map.get_rect().size
+map = pygame.image.load(r'images/abstract_map.jpg') # download map
+display_width, display_height = map.get_rect().size # width and height initialization
 
 display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Drone')
 clock = pygame.time.Clock()
-
+# download images
 drone_img = [pygame.image.load(r'images/drone1.png'), pygame.image.load(r'images/drone2.png')]
 drone_img2 = [pygame.image.load(r'images/drone1.png'), pygame.image.load(r'images/drone2.png')]
 
@@ -18,10 +18,11 @@ base_img = [pygame.image.load(r'images/trash1.png'), pygame.image.load(r'images/
             pygame.image.load(r'images/trash3.png'), pygame.image.load(r'images/trash4.png'),
             pygame.image.load(r'images/trash5.png')]
 img_counter = 0
-
+# trash type initialization
 trash = [{'paper': []}, {'plastic': []}, {'organic': []}]
 
 
+# common class for all objects
 class Object:
     def __init__(self, x, y, image):
         self.x = x
@@ -32,6 +33,7 @@ class Object:
         display.blit(self.image, (self.x - self.image.get_rect().size[0] // 2, self.y - self.image.get_rect().size[1] // 2))
 
 
+# main trash bin initialization
 class Base(Object):
     def __init__(self, x, y, image, capacity):
         Object.__init__(self, x, y, image)
@@ -41,6 +43,7 @@ class Base(Object):
     def draw(self):
         display.blit(base_img[self.current_capacity], (self.x - self.image.get_rect().size[0] // 2, self.y - self.image.get_rect().size[1]))
 
+    # changing capacity of trash bin
     def change_capacity(self, size):
         if 1 <= size <= self.capacity - self.current_capacity:
             self.current_capacity += size
@@ -48,9 +51,11 @@ class Base(Object):
             self.current_capacity = 0
 
 
+# Drone initialization
 class Drone(Object):
     def __init__(self, x, y, image):
         Object.__init__(self, x, y, image)
+        # list of places where drone must go
         self.goals = []
         self.speed = 1
         self.based = False
@@ -60,6 +65,7 @@ class Drone(Object):
         if img_counter == 12:
             img_counter = 0
 
+        # if dorne has goals and not on the base he moves
         if self.goals and not self.based:
             if self.x + self.image.get_rect().size[0] // 2 < self.goals[0][0]:
                 self.x += self.speed
@@ -82,6 +88,7 @@ class Drone(Object):
     def go_to(self, x, y):
         self.goals.append((x, y))
 
+    # nearest trash adds to drone's goals each category
     def get_trash(self):
         for i in range(len(trash)):
             for key, value in trash[i].items():
@@ -89,7 +96,7 @@ class Drone(Object):
                 for elem in value:
                     cords.append((elem.x, elem.y))
                 sorted_cords = sort_cords(self.x, self.y, cords)
-                print(sorted_cords)
+                # after each category drone goes to base
                 for i in range(len(sorted_cords) // 2):
                     self.go_to(sorted_cords[i * 2], sorted_cords[i * 2 + 1])
             self.go_to(display_width // 2, display_height // 2)
@@ -97,6 +104,7 @@ class Drone(Object):
         return
 
 
+# creating trash objects with random coordinates
 def create_trash(array):
     for i in range(len(array)):
         for key in array[i].keys():
@@ -112,6 +120,7 @@ def draw_trash(array):
                 obj.draw()
 
 
+# sort array regarding given coordinates
 def sort_cords(x, y, array):
     if len(array) <= 1:
         return array[0][0], array[0][1]
@@ -127,6 +136,7 @@ def sort_cords(x, y, array):
     return result
 
 
+# delete trash object from array
 def delete_trash(x, y):
     global trash
     for i in range(len(trash)):
@@ -139,33 +149,32 @@ def delete_trash(x, y):
                 value.pop(temp_i)
 
 
-
 def main():
     create_trash(trash)
+    # bool shows is where second drone on the map
     second = False
     base = Base(display_width // 2, display_height // 2, base_img[0], 4)
-    #drone = Drone(display_width // 2, display_height // 2 - 60, drone_img[0])
-    drone = Drone(100, 100, drone_img[0])
+    drone = Drone(display_width // 2, display_height // 2 - 160, drone_img[0])
     drone2 = Drone(display_width // 2, -100, drone_img[0])
     drone2.speed = 2
     start = True
+    drone.get_trash()
     while start:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    drone.get_trash()
         display.blit(map, (0, 0))
         base.draw()
         draw_trash(trash)
         drone.draw()
         if drone.based:
             second = True
+            # change base capacity if drone is on the base
             base.change_capacity(4)
             if len(drone2.goals) == 0:
                 drone2.go_to(display_width // 2, display_height // 2 - 90)
+            # if second drone is on the base
             if drone2.y == 226:
                 base.change_capacity(-1)
                 drone2.goals = []
@@ -173,6 +182,7 @@ def main():
                 drone.based = False
         if second:
             drone2.draw()
+            # if second drone is out of screen
             if drone2.y == -214:
                 second = False
                 drone2.goals = []
